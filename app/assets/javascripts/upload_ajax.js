9a89f5b0d1c79cba6da7834b58605a9a00af4ea8
Drew Finstrom <drew@finstrom.us>
2015-08-18 12:12:54 -0700

    componentize async image upload

---
+++ app/assets/javascripts/upload_ajax.js
@@ -0,0 +1,74 @@
+// ajax uploading to S3
+
+$.support.cors = true;
+
+$(document).ready(function() {
+
+  // trigger on file select
+  $(document).on('change', '.btn-file :file', function() {
+    console.log('firing trigger for file select');
+    var input = $(this);
+    var numFiles = input.get(0).files ? input.get(0).files.length : 1;
+    var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+
+    $("#s3-image-uploader-filename").val(label);
+    if (label === '') {
+      $('#s3-image-uploader-submit-btn').prop('disabled', true);
+    } else {
+      $('#s3-image-uploader-submit-btn').prop('disabled', false);
+    }
+    console.log('done firing trigger for file select');
+  });
+
+  // toggle the modal and collect the upload-group
+  $("button[data-ajax-img-upload-group]").on('click', function() {
+    var uploadGroup = $(this).attr('data-ajax-img-upload-group');
+    console.log("setting upload group to: " + uploadGroup);
+    $("#upload-group.s3-ajax-uploader").val(uploadGroup);
+    // trigger modal
+    jQuery.noConflict();
+    $('#ajax-img-upload').modal('show');
+  });
+
+  $('form.s3-ajax-uploader').submit(function(event) {
+    event.preventDefault();
+    // dismiss modal
+    $('#ajax-img-upload').modal('hide');
+    // collect data from the image fields
+    var uploadGroup = $("#upload-group.s3-ajax-uploader").val();
+    console.log("got upload group: " + uploadGroup);
+    var imgContainerDiv = $("div[data-ajax-img-upload-group='" + uploadGroup + "']");
+    var imgUrlInput = $("input[data-ajax-img-upload-group='" + uploadGroup + "']");
+    // collect form data from form
+    var formData = new FormData($(this)[0]);
+    // set the image to a loading spinner
+    imgContainerDiv.html('<img src="/assets/ajax-loader.gif" alt="loading.." />');
+    console.log("submitting image upload..")
+    var startTimer = new Date();
+    var jqxhr = $.ajax({
+      url         : $(this).attr('action'),
+      type        : $(this).attr('method'),
+      contentType: false,
+      processData: false,
+      data        : formData})
+    .done(function( data, textStatus, jqXHR ) {
+      console.log('AJAX image upload done');
+      var image_name = $("label[for='file']").text()
+      var image_path = decodeURIComponent(jqXHR.getResponseHeader('Location'));
+      imgContainerDiv.html('<img src="' + image_path + '" alt="' + image_name + '" />');
+      console.log('URL to image: ' + image_path);
+      imgUrlInput.val(image_path);
+      var stopTimer = new Date();
+      console.log("upload took approx. " + (stopTimer - startTimer) / 1000 + " seconds.");
+    })
+    .fail(function( jqXHR, textStatus, errorThrown ) {
+      alert('AJAX image upload error');
+      console.log(textStatus + "\n" + errorThrown);
+    })
+    .always(function( jqXHR, textStatus, errorThrown ) {
+      console.log('AJAX image upload complete');
+      $('input[type="submit"][value="Upload Image"]').prop('disabled', true);
+    });
+    return false;
+  });
+});
